ea8445182ab31ec231f402780f4105a8cfa8e6d0
Natanael <natanaelsilva.progjs@gmail.com>
2022-08-31 00:03:01 -0300

    feat: Implementing select_user in UserRepositoryclear

--- src/infra/repositories/user_repository_test.py
+++ src/infra/repositories/user_repository_test.py
@@ -1,11 +1,13 @@
 from faker import Faker
 
 from src.infra.config import DBConnectionHandler
+from src.infra.entities import Users as UsersEntity
 from .user_repository import UserRepository
+
 
 faker = Faker()
 user_repository = UserRepository()
-db_connection = DBConnectionHandler()
+db_connection_handler = DBConnectionHandler()
 
 
 def test_insert_user():
@@ -14,7 +16,7 @@ def test_insert_user():
     """
     name = faker.name()
     password = faker.password()
-    engine = db_connection.get_engine()
+    engine = db_connection_handler.get_engine()
 
     new_user = user_repository.insert_user(name, password)
     query = engine.execute(f"SELECT * FROM users WHERE id = {new_user.id}").fetchone()
@@ -25,3 +27,25 @@ def test_insert_user():
     assert new_user.id == query.id
     assert new_user.name == query.name
     assert new_user.password == query.password
+
+
+def test_select_user():
+    """Shoul select a user in Users table and compare it"""
+
+    user_id = faker.random_number(digits=5)
+    name = faker.name()
+    password = faker.word()
+    data = UsersEntity(id=user_id, name=name, password=password)
+
+    engine = db_connection_handler.get_engine()
+    engine.execute(f"INSERT INTO users (id, name, password) VALUES ('{user_id}','{name}','{password}');")
+
+    query_user_one = user_repository.select_user(user_id=user_id)
+    query_user_two = user_repository.select_user(name=name)
+    query_user_thee = user_repository.select_user(user_id=user_id, name=name)
+
+    assert data in query_user_one
+    assert data in query_user_two
+    assert data in query_user_thee
+
+    engine.execute(f"DELETE FROM users WHERE id='{user_id}';")
