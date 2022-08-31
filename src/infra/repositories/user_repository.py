08ea53113d105f85f815d1eb367ac3bb29a0076c
Natanael <natanaelsilva.progjs@gmail.com>
2022-08-30 22:57:38 -0300

    feat: Implementing models in domain layer

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,7 +1,7 @@
-from collections import namedtuple
 from src.infra.config import DBConnectionHandler
 
-from src.infra.entities import Users
+from src.domain.models import Users
+from src.infra.entities import Users as UsersEntity
 
 
 class UserRepository:
@@ -16,15 +16,13 @@ class UserRepository:
         :return : User object
         """
 
-        InsertData = namedtuple("Users", "id name password")
-
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password=password)
+                new_user = UsersEntity(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
 
-                return InsertData(
+                return Users(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
 
